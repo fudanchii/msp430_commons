22350ba46b9db038130e33ec29ef909358784eb0
Nurahmadie <nurahmadie@gmail.com>
2015-01-02 11:20:01 +0700

    fix indentation

--- msp430_commons.c
+++ msp430_commons.c
@@ -16,30 +16,30 @@
 extern unsigned int duty_cycle_at[];
 
 static inline void _PWM1_std_setup(int period, int duty_cycle, int ie) {
-	TA0CTL	 = TASSEL_2 | MC_1;			// SMCLK, upmode
+	TA0CTL   = TASSEL_2 | MC_1;			// SMCLK, upmode
 	TA0CCTL1 = OUTMOD_7 | ie;			// TACCR1 reset/set
 	TA0CCR0  = period-1;				// PWM Period
-	TA0CCR1	 = duty_cycle;				// TACCR1 initial PWM Duty Cycle
+	TA0CCR1  = duty_cycle;				// TACCR1 initial PWM Duty Cycle
 }
 
 inline void Pin1_select_peripheral(int bits) {
-	P1DIR	|= bits;					// Set <bits> direction (output)
-	P1SEL	|= bits;					// Select <bits> for peripheral use
+	P1DIR |= bits;					// Set <bits> direction (output)
+	P1SEL |= bits;					// Select <bits> for peripheral use
 }
 
 inline void Pin1_switch_setup(int bits) {
-    // Enable interrupt on Pin 1.<bits>
-    // on Hi-Lo transition
-    P1IE  |= bits;
-    P1IES |= bits;
+	// Enable interrupt on Pin 1.<bits>
+	// on Hi-Lo transition
+	P1IE  |= bits;
+	P1IES |= bits;
 
-    // Enable pull-up resistor on Pin 1.<bits>
-    P1OUT |= bits;
-    P1REN |= bits;
+	// Enable pull-up resistor on Pin 1.<bits>
+	P1OUT |= bits;
+	P1REN |= bits;
 
-    // Clear interrupt flag on Pin 1.<bits>
-    // so we can accept more interrupt
-    P1IFG &= ~bits;
+	// Clear interrupt flag on Pin 1.<bits>
+	// so we can accept more interrupt
+	P1IFG &= ~bits;
 }
 
 inline void Pin1_setup(int direction, int bits) {
@@ -73,8 +73,8 @@ inline void init_servo_angle_lut() {
 	unsigned int servo_stepval, servo_stepnow;
 
 	// Calculate the step value and define the current step, defaults to maximum.
-	servo_stepval 	= ((SERVO_MAX - SERVO_MIN) / SERVO_STEPS);
-	servo_stepnow	= SERVO_MAX;
+	servo_stepval = ((SERVO_MAX - SERVO_MIN) / SERVO_STEPS);
+	servo_stepnow = SERVO_MAX;
 
 	// Fill up the LUT
 	for (; i < SERVO_STEPS + 1; i++) {
